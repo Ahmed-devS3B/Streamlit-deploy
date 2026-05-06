4a06876757e84a81411eb287d6ec392637690776
Ahmed Ibrahim <Ahmedelgamal0596@gmail.com>
2026-05-06 22:42:34 +0300

    update

--- app.py
+++ app.py
@@ -1,8 +1,17 @@
 import streamlit as st
 
-st.title("Hello, SI ?")
+st.title("Hello, SI 👋")
 
-name = st.text_input("What is your name ?")
+name = st.text_input("What's your name ?")
 
 if name:
     st.write(f"Welcome, {name.title()} 👋")
+
+age = st.slider("What's your age ?", 0, 100, 25)
+
+if age < 18:
+    st.write("You are a kid.")
+elif age > 18 and age < 65:
+    st.write("You are an adult.")
+else:
+    st.write("You are a senior.")
